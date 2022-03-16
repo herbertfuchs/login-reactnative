c1aa1ad866117f3a330b90a827a6eb381d619db6
herbert.fuchs <hmcfuchs@gmail.com>
2022-03-16 15:15:32 -0300

    Construção da primeira tela e animação da logo via animatable

--- src/Pages/Welcome/index.js
+++ src/Pages/Welcome/index.js
@@ -6,22 +6,75 @@ import {
     Image,
     TouchableOpacity } from 'react-native';
 
+import * as Animatable from 'react-native-animatable';
+
 export default function Welcome(){
     return(
         <View style={styles.container}>
-            <View>
-                <Image 
+
+            <View style={styles.containerLogo}>
+                <Animatable.Image
+                    animation="flipInY"
                     source={require('../../assets/logo.png')}
                     style={{ width: '100%' }} 
                     resizeMode="contain"
                 />
             </View>
+            <View style={styles.containerForm}>
+                <Text style={styles.title}>Monitore, organize seus gastos de qualquer lugar!</Text>
+                <Text style={styles.text}>Faça o login para começar</Text>
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Acessar</Text>
+                </TouchableOpacity>
+            </View>
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-
+        flex: 1,
+        backgroundColor: "#38a69d"
     },
+    containerLogo: {
+        flex: 2,
+        backgroundColor: "#38a69d",
+        justifyContent: 'center', // Alinha componentes-filho horizontalmente ao centro.
+        alignItems: 'center' // Alinha componentes-filho verticalmente ao centro.
+    },
+    containerForm: {
+        flex: 1,
+        backgroundColor: '#fff',
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        paddingStart: '5%',
+        paddingEnd: '5%'
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 28,
+        marginBottom: 12
+    },
+    text: {
+        color: "#a1a1a1"
+    },
+    button: {
+        position: 'absolute',
+        backgroundColor: '#38a69d',
+        borderRadius: 50,
+        paddingVertical: 8,
+        width: '60%',
+        alignSelf: 'center',
+        bottom: '15%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText: {
+        fontSize: 18,
+        color: "#fff",
+        fontWeight: 'bold'
+    }
 });
